3a1314a68f2e1bb627945cae8fd3762b1f93ea70
LENOVOW540\jez <jez@kingjez.com>
2017-12-01 23:18:22 +0000

    Added test Blynk integration. Added a basic counter and Blynk app callback. Also ensured that Blynk auth key is stored in a separate .h file (BlynkAuth.h) that defines the key as char BlynkAuth[] = "123456789absdef1234..."; This file is also added to .gitignore so that it's not uploaded to public repo by mistake.

--- src/HeatingControl.ino
+++ src/HeatingControl.ino
@@ -1,18 +1,34 @@
-/*
- * Project HeatingControl
- * Description:
- * Author:
- * Date:
- */
 
-// setup() runs once, when the device is first turned on.
-void setup() {
-  // Put initialization like pinMode and begin functions here.
+#define BLYNK_PRINT Serial  // Set serial output for debug prints
+//#define BLYNK_DEBUG       // Uncomment this to see detailed prints
 
+#include <blynk.h>
+#include "BlynkAuth.h"
+
+
+// Attach a Button widget (mode: Switch) to the Digital pin 7 - and control the built-in blue led.
+// Attach a Graph widget to Analog pin 1
+// Attach a Gauge widget to Analog pin 2
+int i = 0, j = 0;
+
+void setup()
+{
+    Serial.begin(9600);
+    delay(5000); // Allow board to settle
+
+    Blynk.begin(BlynkAuth);
 }
 
-// loop() runs over and over again, as quickly as it can execute.
-void loop() {
-  // The core of your code will likely live here.
+// Attach a Button widget (mode: Push) to the Virtual pin 1 - and send sweet tweets!
+BLYNK_WRITE(V4) {
+    j = param.asInt();
+}
 
+void loop()
+{
+    i++;
+    Blynk.virtualWrite(V2, i);
+    Serial.printf("%d\n", j);
+    Blynk.run();
+    delay(1000);
 }
