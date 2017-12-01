0dff51f7bca15b8f3c444c4e892decd1143aa751
LENOVOW540\jez <jez@kingjez.com>
2017-12-01 22:59:02 +0000

    Adding time stamping.

--- src/HeatingControl.ino
+++ src/HeatingControl.ino
@@ -1,9 +1,11 @@
 
 float remoteTemperature = -999;
+time_t timeStamp = 0;
 
 // Cloud functions must return int and take one String
 int retrieveRemoteTemperature(String extra) {
   remoteTemperature = atof(extra);
+  timeStamp = Time.now();
   return 1;
 }
 
@@ -28,7 +30,9 @@ void loop()
 {
   Serial.println("******************");
 
-  Serial.printf("remoteTemp: %3.4f \n", remoteTemperature);
+  Serial.printf("remoteTemp at %i: %3.4f \n", timeStamp, remoteTemperature);
 
+  int age = Time.now() - timeStamp;
+  Serial.printf("Age: %d\n", age);
   delay(1000);
 }
