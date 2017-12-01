29c235ccfe03dba40d62ba6301db943480febd57
LENOVOW540\jez <jez@kingjez.com>
2017-12-01 22:58:49 +0000

    Retrieving a remote temperature. Appears stable after 5 min testing.

--- src/HeatingControl.ino
+++ src/HeatingControl.ino
@@ -1,18 +1,34 @@
-/*
- * Project HeatingControl
- * Description:
- * Author:
- * Date:
- */
 
-// setup() runs once, when the device is first turned on.
-void setup() {
-  // Put initialization like pinMode and begin functions here.
+float remoteTemperature = -999;
 
+// Cloud functions must return int and take one String
+int retrieveRemoteTemperature(String extra) {
+  remoteTemperature = atof(extra);
+  return 1;
 }
 
-// loop() runs over and over again, as quickly as it can execute.
-void loop() {
-  // The core of your code will likely live here.
+void setup()
+{
+  // Setup IO
+  pinMode(A0, INPUT);     // ADC for temp sensor
+  pinMode(D0, OUTPUT);    // D0 for Zone 1 control
+  digitalWrite(D0, LOW);  // Zone 1 off.
 
+  Serial.begin(9600);
+
+  bool success = Particle.function("postTemp", retrieveRemoteTemperature);
+
+  // Allow board to settle
+  delay(5000);
+
+  Particle.publish("Waking up");
+}
+
+void loop()
+{
+  Serial.println("******************");
+
+  Serial.printf("remoteTemp: %3.4f \n", remoteTemperature);
+
+  delay(1000);
 }
